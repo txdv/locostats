da570b1ac106ed5b462d97e27f619976adbb4502
Andrius Bentkus <andrius.bentkus@rwth-aachen.de>
2010-10-28 20:08:57 +0200

    Add create_irc to associate a nickname in an irc server with the profile/username.

--- lib/locostats/activerecord/irc.rb
+++ lib/locostats/activerecord/irc.rb
@@ -1,6 +1,38 @@
 require 'locostats/activerecord/default'
 
 module PsychoStats::Site
+
+  class User
+
+    # creates a user and associates it with the profile
+    # sets the profile irc value to username, which should be the nickname in irc
+    def self.create_irc(username, password, profile)
+
+      user = User.new
+      max = User.maximum(:userid)
+      if max.nil?
+        max = 1
+      else
+        user.id = max + 1
+      end
+      user.username = username
+      user.password = Digest::MD5.hexdigest(password)
+
+      # Todo: make some default settings for this
+      user.confirmed = 1
+      user.accesslevel = 2
+      user.save!
+
+      profile.userid = user.id
+      profile.irc = username
+      profile.save!
+      return user
+
+    rescue
+      nil
+    end
+
+  end
   
   module Irc
 
@@ -13,6 +45,7 @@ module PsychoStats::Site
         remove_column PsychoStats::Site::Profile.table_name, :irc
       end
     end
+
   end
 
 end
